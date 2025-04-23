70cc19b265466431c9afe8bd29b0b4ec6696d8b1
Stefan Reinauer <stefan.reinauer@coreboot.org>
2025-04-23 15:25:33 -0700

    Implement autoconfig (courtesy Matt Harlum)

--- RTL/autoconfig.v
+++ RTL/autoconfig.v
@@ -1,14 +1,103 @@
+/*
+Copyright 2022 Matthew Harlum
+
+Licensed under
+Creative Commons Attribution-ShareAlike 4.0 International License.
+
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+*/
+
 module Autoconfig (
-    input AS_n,
+    input autoconfig_cycle,
+    input [6:0] ADDRL,
+    input FCS_n,
+    input CLK,
+    input READ,
+    input [3:0] DIN,
     input RESET_n,
-    output reg cfgout
+    input [1:0] z3_state,
+    output reg [3:0] scsi_base_addr,
+    output reg CFGOUT_n,
+    output reg dtack,
+    output reg configured,
+    output reg shutup,
+    output reg [3:0] DOUT
 );
 
-always @(posedge AS_n or negedge RESET_n) begin
+localparam  Z3_IDLE  = 2'd0,
+            Z3_START = 2'd1,
+            Z3_DATA  = 2'd2,
+            Z3_END   = 2'd3;
+
+`ifndef makedefines
+`define SERIAL 32'd0
+`endif
+
+localparam [15:0] mfg_id  = 16'd514; // Open Amiga hardware registry ID https://github.com/oahr/oahr
+localparam [7:0]  prod_id = 8'd84;
+localparam [31:0] serial  = `SERIAL;
+
+// Register Config in/out at end of bus cycle
+always @(posedge FCS_n or negedge RESET_n)
+begin
   if (!RESET_n) begin
-    cfgout <= 0;
+    CFGOUT_n <= 1'b1;
   end else begin
-    cfgout <= 1;
+    CFGOUT_n <= !configured && !shutup;
+  end
+end
+
+always @(posedge CLK or negedge RESET_n)
+begin
+  if (!RESET_n) begin
+    DOUT[3:0]          <= 4'b0;
+    configured         <= 1'b0;
+    dtack              <= 1'b0;
+    shutup             <= 1'b0;
+    scsi_base_addr[3:0] <= 4'b0000;
+  end else if (z3_state == Z3_DATA && autoconfig_cycle == 1) begin
+    dtack <= 1;
+    if (READ) begin
+      case ({ADDRL[5:0],ADDRL[6]})
+        7'h00:   DOUT[3:0] <= 4'b1001;        // Type: Zorro III
+        7'h01:   DOUT[3:0] <= 4'b0000;        // 16 MB
+        7'h02:   DOUT[3:0] <= ~prod_id[7:4];  // Product number
+        7'h03:   DOUT[3:0] <= ~prod_id[3:0];  // Product number
+        7'h04:   DOUT[3:0] <= ~4'b0011;       // IO device, Size Extension, Zorro III
+        7'h05:   DOUT[3:0] <= ~4'b0001;       // Automatically sized by OS ?
+        7'h08:   DOUT[3:0] <= ~mfg_id[15:12]; // Manufacturer ID
+        7'h09:   DOUT[3:0] <= ~mfg_id[11:8];  // Manufacturer ID
+        7'h0A:   DOUT[3:0] <= ~mfg_id[7:4];   // Manufacturer ID
+        7'h0B:   DOUT[3:0] <= ~mfg_id[3:0];   // Manufacturer ID
+        7'h0C:   DOUT[3:0] <= ~serial[31:28]; // Serial number
+        7'h0D:   DOUT[3:0] <= ~serial[27:24]; // Serial number
+        7'h0E:   DOUT[3:0] <= ~serial[23:20]; // Serial number
+        7'h0F:   DOUT[3:0] <= ~serial[19:16]; // Serial number
+        7'h10:   DOUT[3:0] <= ~serial[15:12]; // Serial number
+        7'h11:   DOUT[3:0] <= ~serial[11:8];  // Serial number
+        7'h12:   DOUT[3:0] <= ~serial[7:4];   // Serial number
+        7'h13:   DOUT[3:0] <= ~serial[3:0];   // Serial number
+        7'h14:   DOUT[3:0] <= ~4'b0000;       // ROM vector
+        7'h15:   DOUT[3:0] <= ~4'b0010;       // ROM vector
+        7'h16:   DOUT[3:0] <= ~4'b0000;       // ROM vector
+        7'h17:   DOUT[3:0] <= ~4'b0000;       // ROM vector
+        7'h20:   DOUT[3:0] <= 4'b0;
+        7'h21:   DOUT[3:0] <= 4'b0;
+        default: DOUT[3:0] <= 4'hF;
+      endcase
+    end else begin
+      if (ADDRL[5:0] == 6'h13) begin
+        // Shutup
+        shutup <= 1;
+      end else if (ADDRL[5:0] == 6'h11) begin
+        // Write base address
+        scsi_base_addr <= DIN[3:0];
+        configured    <= 1;
+      end
+    end
+  end else begin
+    dtack <= 0;
   end
 end
 
--- RTL/top.v
+++ RTL/top.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 
 module A4092(
-    input [31:0] ADDR,   // Only bits [8:0], 17,18,19, and 23-31 are used
+    input [31:0] A,   // Only bits [8:0], 17,18,19, and 23-31 are used
     input CLK_50M,
     output AS_n,
     input PLD_DS_n,
     output [3:0] DS_n,
-    input IORST,
+    input IORST_n,
     input [2:0] FC,
     input Z_7M,
     input FCS_n,
@@ -26,7 +26,7 @@ module A4092(
     output ABOEH_n,
     output D2Z_n,
     output Z2D_n,
-    input MASTER,
+    output reg MASTER,
     input SLACK,
     input NCR_INT,
     input SREG,
@@ -51,15 +51,156 @@ module A4092(
     output CINH_n
     );
 
+`include "globalparams.vh"
+
+// Synchronizers
+reg [1:0] DS0_n_sync;
+reg [1:0] DS1_n_sync;
+reg [1:0] DS2_n_sync;
+reg [1:0] DS3_n_sync;
+reg [1:0] FCS_n_sync;
+
+always @(posedge CLK or negedge IORST_n)
+begin
+  if (!IORST_n) begin
+    DS0_n_sync[1:0] <= 2'b11;
+    DS1_n_sync[1:0] <= 2'b11;
+    DS2_n_sync[1:0] <= 2'b11;
+    DS3_n_sync[1:0] <= 2'b11;
+    FCS_n_sync[1:0] <= 2'b11;
+  end else begin
+    DS0_n_sync[1:0] <= {DS0_n_sync[0], DS_n[0]};
+    DS1_n_sync[1:0] <= {DS1_n_sync[0], DS_n[1]};
+    DS2_n_sync[1:0] <= {DS2_n_sync[0], DS_n[2]};
+    DS3_n_sync[1:0] <= {DS3_n_sync[0], DS_n[3]};
+    FCS_n_sync[1:0] <= {FCS_n_sync[0], FCS_n};
+  end
+end
+
+// Autoconf
+wire [3:0] autoconfig_dout;
+wire autoconfig_cfgout;
+
+wire [3:0] scsi_base_addr;
+
+reg [27:8] ADDR;
+reg autoconfig_addr_match;
+reg scsi_addr_match;
+
+wire match = autoconfig_addr_match || scsi_addr_match;
+wire configured;
+wire validspace = FC[1] ^ FC[0]; // 1 when FC indicates user/supervisor data/program space
+wire shutup;
+
+// Latch address bits 27-8 on FCS_n asserted
+// 
+always @(negedge FCS_n or negedge IORST_n)
+begin
+  if (!IORST_n) begin
+    ADDR                  <= 20'b0;
+    scsi_addr_match        <= 0;
+    autoconfig_addr_match <= 0;
+  end else begin
+    MASTER <= READ; // CEAB for address bits
+    ADDR[27:8] <= A[27:8];
+
+    if (A[31:28] == scsi_base_addr && configured) begin
+      scsi_addr_match <= 1;
+    end else begin
+      scsi_addr_match <= 0;
+    end
+
+    if ({A[31:24]} == 8'hFF && !configured && !shutup && !CFGIN_n) begin
+      autoconfig_addr_match <= 1;
+    end else begin
+      autoconfig_addr_match <= 0;
+    end
+  end
+end
+
+reg [1:0] z3_state;
+reg dtack;
+reg scsi_cycle;
+reg autoconfig_cycle;
+wire autoconfig_dtack;
+wire scsi_dtack;
+
+always @(posedge CLK or negedge IORST_n)
+begin
+  if (!IORST_n) begin
+    z3_state         <= Z3_IDLE;
+    dtack            <= 1'b0;
+    scsi_cycle        <= 1'b0;
+    autoconfig_cycle <= 1'b0;
+  end else begin
+    case (z3_state)
+      Z3_IDLE:
+        begin
+          dtack <= 0;
+          if (!FCS_n_sync[1] && match && validspace) begin
+            z3_state         <= Z3_START;
+            autoconfig_cycle <= autoconfig_addr_match;
+            scsi_cycle        <= scsi_addr_match;
+          end else begin
+            autoconfig_cycle <= 0;
+            scsi_cycle        <= 0;
+            z3_state         <= Z3_IDLE;
+          end
+        end
+      Z3_START:
+        begin
+          if (FCS_n_sync[1]) begin
+            z3_state <= Z3_IDLE;
+          end else if (READ || (!DS0_n_sync[1] || !DS1_n_sync[1] || !DS2_n_sync[1] || !DS3_n_sync[1]) && DOE) begin
+            z3_state <= Z3_DATA;
+          end else begin
+            z3_state <= Z3_START;
+          end
+        end
+      Z3_DATA:
+        begin
+          if (FCS_n_sync[1]) begin
+            z3_state <= Z3_IDLE;
+          end else if (autoconfig_dtack && autoconfig_cycle || scsi_dtack && scsi_cycle) begin
+            z3_state <= Z3_END;
+          end
+        end
+      Z3_END:
+        begin
+          if (FCS_n_sync[1]) begin
+            z3_state <= Z3_IDLE;
+            scsi_cycle <= 0;
+            autoconfig_cycle <= 0;
+            dtack <= 0;
+          end else begin
+            z3_state <= Z3_END;
+            dtack <= 1;
+          end
+        end
+    endcase
+  end
+end
+
 Autoconfig AUTOCONFIG (
-  .AS_n (AS_n),
-  .RESET_n (IORST),
-  .cfgout (CFGOUT_n)
+  .scsi_base_addr (scsi_base_addr),
+  .ADDRL ({ADDR[8], A[7:2]}),
+  .FCS_n (FCS_n_sync[1]),
+  .CLK (CLK),
+  .READ (READ),
+  .DIN (D[31:28]),
+  .RESET_n (IORST_n),
+  .CFGOUT_n (autoconfig_cfgout),
+  .autoconfig_cycle (autoconfig_cycle),
+  .dtack (autoconfig_dtack),
+  .configured (configured),
+  .DOUT (autoconfig_dout),
+  .z3_state (z3_state),
+  .shutup (shutup)
 );
 
 SCSI SCSI (
   .CLK (CLK_50M),
-  .RESET_n (IORST),
+  .RESET_n (IORST_n),
   .DTACK (DTACK_n)
 );
 
